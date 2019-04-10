24624e42fef891e2ce81a83beb784b7e88948fc0
Mete Köse <mete.kose@isbank.com.tr>
2019-04-10 15:53:47 +0300

    initial commit

---
+++ ScreenShot.ps1
@@ -0,0 +1,17 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+#dimensions
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1024, 768)
+#output_path
+screenshot $bounds "C:\screenshot.png"
